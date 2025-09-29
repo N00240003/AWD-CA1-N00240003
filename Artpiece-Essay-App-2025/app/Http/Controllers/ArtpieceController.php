ab13936256a088947d8fcf826a53272d3e190038
N00240003 <N00240003@iadt.ie>
2025-09-29 15:00:07 +0100

    Re-created project, was busy migrating

---
+++ Artpiece-Essay-App-2025/app/Http/Controllers/ArtpieceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Artpiece;
+use Illuminate\Http\Request;
+
+class ArtpieceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Artpiece $artpiece)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Artpiece $artpiece)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Artpiece $artpiece)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Artpiece $artpiece)
+    {
+        //
+    }
+}
