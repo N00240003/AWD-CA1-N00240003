20f1d95a579615b872b10fcd046d16910349b9ec
N00240003 <N00240003@iadt.ie>
2025-10-20 13:29:20 +0100

    Started READ ME and adding more comments to code

--- Artpiece-Essay-App-2025/routes/web.php
+++ Artpiece-Essay-App-2025/routes/web.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\ArtpieceController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+// Welcome page route
 Route::get('/', function () {
     return view('welcome');
 });
-
+// Dashboard route
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -17,19 +17,28 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    //Routes for Index, Create and Show {Artpieces}
     Route::get('/artpieces', [ArtpieceController::class, 'index'])->name('artpieces.index');
     Route::get('/artpieces/create', [ArtpieceController::class, 'create'])->name('artpieces.create');
     Route::get('/artpieces{artpiece}', [ArtpieceController::class, 'show'])->name('artpieces.show');
     Route::post('/artpieces', [ArtpieceController::class, 'store'])->name('artpieces.store');
 
+    //Routes for Edit, Update and Delete {Artpieces}
     Route::get('/artpieces/{artpiece}/edit', [ArtpieceController::class, 'edit'])->name('artpieces.edit');
     Route::put('/artpieces/{artpiece}', [ArtpieceController::class, 'update'])->name('artpieces.update');
     Route::delete('/artpieces/{artpiece}', [ArtpieceController::class, 'destroy'])->name('artpieces.destroy');
 
-    Route::get('/', 'ArtpieceController@index');
-    // Route::get('/search', 'ArtpieceController@search')->name('artpieces.search');
-     Route::get('/search', [ArtpieceController::class, 'search'])->name('artpieces.search');
+    // Attempted a tutorial
+    // I added the following to lines of code in an attempt to add a search function to my project
 
+    // This one would not work anyway as I am already using the route for my 'Welcome' page (see line 7-9) 
+    //Route::get('/', 'ArtpieceController@index');
+    //
+    // The tutorial uses an unfamiliar syntax, so the logic may be correct but is just not meshing with the rest of my code 
+    //Route::get('/search', 'ArtpieceController@search')->name('artpieces.search');
+
+    //Final route for my search function
+    Route::get('/search', [ArtpieceController::class, 'search'])->name('artpieces.search');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
---
+++ readme.txt
@@ -0,0 +1,20 @@
+//Ciara Conroy
+//N00240003
+//AWD - CA1 : Start of Artpiece-Essay-App-2025
+
+
+
+Topic=Model : Artpieces
+-mvc
+-A controller is the 'middleman': it decides what action is needed, asks the Model to talk to the database, and then passes the data to the View for display.
+-migrations
+-A view is what the user sees in their browser. It usually contains HTML mixed with Blade syntax (like @foreach) to display dynamic data.
+-seeder
+-Blade tmplate
+-demonstrate an understanding of advanced Laravel feature – ie. topics learned through independent research.
+Explain the principles oserver-side Web Application Frameworks
+-Debugging
+  <a> within an <a>
+
+-Eloquent
+@props - defines the properties that may be passed into the component (I pass these in from the view, make sure the names match)
